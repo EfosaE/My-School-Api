b01e29ba21fcd25774f8f14f90d41bf9dfbefec0
EfosaE <osamwonyiefosa02@gmail.com>
2024-07-02 19:13:05 +0100

    Stop tracking dist folder

--- dist/routes/studentRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const studentController_1 = require("../controllers/studentController");
-const studentRouter = (0, express_1.Router)();
-studentRouter.route('/').post(studentController_1.createStudent);
-studentRouter.route('/').get(studentController_1.getAllStudents);
-exports.default = studentRouter;
