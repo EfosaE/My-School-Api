9d1e559691fb887412bc87e7a800cd59b93c288f
EfosaE <osamwonyiefosa02@gmail.com>
2024-07-06 11:53:06 +0100

    refactored the prismaclient

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,8 +1,5 @@
-import { Prisma, PrismaClient } from "@prisma/client";
 import { Request, Response } from 'express';
-
-const prisma = new PrismaClient();
-
+import prisma from '../utils/prismaClient';
 
 export async function createStudent(req: Request, res: Response) {
   try {
@@ -28,25 +25,24 @@ export async function createStudent(req: Request, res: Response) {
   } finally {
     await prisma.$disconnect();
   }
-  
 }
 
 export async function getAllStudents(req: Request, res: Response) {
-    try {
-      const students = await prisma.students.findMany();
-      res.status(200).json({
-        status: 'successful',
-        data: {
-          length: students.length,
-          students,
-        },
-      });
-    } catch (error) {
-      res.status(500).json({
-        status: 'failed',
-        message: `${error}`,
-      });
-    } finally {
-      await prisma.$disconnect();
-    }
+  try {
+    const students = await prisma.students.findMany();
+    res.status(200).json({
+      status: 'successful',
+      data: {
+        length: students.length,
+        students,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'failed',
+      message: `${error}`,
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
