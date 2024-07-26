2da9b023a957cc7036a4ff0ebe90ecdc33bbdb50
EfosaE <osamwonyiefosa02@gmail.com>
2024-07-26 10:25:49 +0100

    changed something

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import app from "./app";
 const port: number = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript + Node.js + Express!');
+  res.send('My school Api');
 });
 
 // Start the server and listen on the specified port
